651db94ce461cd4c089f5aaa69d596c593530dca
Ziad Amr <Zyadamr177@gmail.com>
2025-08-20 15:05:20 +0300

    Week10 Task2: Refactor prop drilling to use Context API (alx-react-app-props)

---
+++ alx-react-app-props/src/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import UserInfo from './UserInfo';
+
+function ProfilePage() {
+  return (
+    <div style={{ padding: 16 }}>
+      <h1>Profile Page</h1>
+      <UserInfo />
+    </div>
+  );
+}
+
+export default ProfilePage;
