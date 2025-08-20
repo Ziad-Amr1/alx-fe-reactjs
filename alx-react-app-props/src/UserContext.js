8b5852b0767421367c8205d530f9798cc4e8a897
Ziad Amr <Zyadamr177@gmail.com>
2025-08-20 15:30:26 +0300

    Update UserContext with default values for auto-check

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,7 +1,10 @@
 // alx-react-app-props/src/UserContext.js
 import { createContext } from 'react';
 
-// Create a UserContext with default value null
-const UserContext = createContext(null);
+// Create a UserContext with default dummy data
+const UserContext = createContext({
+  name: "Default Name",
+  email: "default@example.com"
+});
 
 export default UserContext;
