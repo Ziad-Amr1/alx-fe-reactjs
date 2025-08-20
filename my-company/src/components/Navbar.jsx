6612db65dc037ff2927de60b62cdf146b17be5c5
Ziad Amr <Zyadamr177@gmail.com>
2025-08-20 15:59:19 +0300

    Update Navbar.jax

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ display:'flex', gap:12, padding:12, background:'#222', color:'#fff' }}>
+    <nav style={{ display:'flex', gap:12, padding:12, backgroundColor:'#222', color:'#fff', justifyContent:'center' }}>
       <Link to="/" style={{ color:'#fff', textDecoration:'none' }}>Home</Link>
       <Link to="/about" style={{ color:'#fff', textDecoration:'none' }}>About</Link>
       <Link to="/services" style={{ color:'#fff', textDecoration:'none' }}>Services</Link>
