e02b64ba339ab862975e50a1f6e950700b1775df
Ziad Amr <Zyadamr177@gmail.com>
2025-08-20 15:28:03 +0300

    Fix: Add UserContext.js and wire Context Provider/Consumer for prop drilling task

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,7 @@
+// alx-react-app-props/src/UserContext.js
 import { createContext } from 'react';
 
+// Create a UserContext with default value null
 const UserContext = createContext(null);
 
 export default UserContext;
