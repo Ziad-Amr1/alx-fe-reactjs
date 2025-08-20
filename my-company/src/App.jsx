3149d381d11c0b0a017c6878b2aaa804181a984c
Ziad Amr <Zyadamr177@gmail.com>
2025-08-20 15:48:48 +0300

    Week10 Task3: Create my-company site with React Router and pages

---
+++ my-company/src/App.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <div style={{ maxWidth: 1000, margin: '0 auto', padding: 20 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ my-company/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import { Link } from 'react-router-dom';
+
+function Navbar() {
+  return (
+    <nav style={{ display:'flex', gap:12, padding:12, background:'#222', color:'#fff' }}>
+      <Link to="/" style={{ color:'#fff', textDecoration:'none' }}>Home</Link>
+      <Link to="/about" style={{ color:'#fff', textDecoration:'none' }}>About</Link>
+      <Link to="/services" style={{ color:'#fff', textDecoration:'none' }}>Services</Link>
+      <Link to="/contact" style={{ color:'#fff', textDecoration:'none' }}>Contact</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
